26f815022da9b3e7fe2800218b585e6878eb21f2
emma-nkn <emmacarene7@gmail.com>
2025-11-04 14:12:45 +0100

    emma commit

--- Flash.sql
+++ Flash.sql
@@ -11,3 +11,25 @@ CREATE TABLE users (
     updated_at DATETIME,
     PRIMARY KEY(id_user)
     );    
+
+
+
+-- Partie Emma
+-- ======================= User story 9 ==================
+
+INSERT INTO Message (game_id, user_id, message)
+VALUES (1, 3, "Prêt pour une partie ?");
+
+-- ======================= User story 10 ==================
+SELECT 
+    m.message AS contenu_message,
+    u.pseudo AS auteur,
+    m.created_at AS date_message,
+    CASE 
+        WHEN m.user_id = 2 THEN TRUE 
+        ELSE FALSE 
+    END AS isSender
+FROM Message m
+JOIN Utilisateur u ON m.user_id = u.id
+WHERE m.created_at >= NOW() - INTERVAL 1 DAY
+ORDER BY m.created_at ASC;
